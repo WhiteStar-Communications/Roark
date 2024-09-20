8d454630e814c682ad58afc9984b1241f9c56e9b
Logan Miller <lmille77@whitestar.io>
2024-09-20 10:11:31 -0400

    Update to RKSwiftUIViewController

--- Sources/Roark/RKKit/Views/RKSwiftUIViewController.swift
+++ Sources/Roark/RKKit/Views/RKSwiftUIViewController.swift
@@ -16,6 +16,8 @@ open class RKSwiftUIViewController<V: RKSwiftUIView> : UIViewController,
     //
     // MARK: Life Cycle Properties
     //
+    
+    open var viewModel : RKViewModel!
 
     open var cancellables = Set<AnyCancellable>()
 
@@ -43,6 +45,7 @@ open class RKSwiftUIViewController<V: RKSwiftUIView> : UIViewController,
     
     open func setView(_ view: V) {
         _ = self
+        self.viewModel = view.viewModel
         let hostingController = UIHostingController(rootView: view)
         addChild(hostingController)
         self.view.addSubview(hostingController.view)
@@ -119,17 +122,5 @@ open class RKSwiftUIViewController<V: RKSwiftUIView> : UIViewController,
         }
     }
 
-}
-
-extension RKSwiftUIViewController {
-    public class func instantiateFromStoryboard(storyboardName: String, storyboardId: String) -> Self {
-        return instantiateFromStoryboardHelper(storyboardName: storyboardName, storyboardId: storyboardId)
-    }
-
-    private class func instantiateFromStoryboardHelper<T>(storyboardName: String, storyboardId: String) -> T {
-        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: storyboardId) as! T
-        return controller
-    }
 }
 
