b343973b515ad3f5f02d751928353b97f17a308c
Logan Miller <lmille77@whitestar.io>
2024-10-01 15:15:36 -0400

    Update

--- Sources/Roark/Base/Router.swift
+++ Sources/Roark/Base/Router.swift
@@ -24,7 +24,7 @@ public protocol Routable: AnyObject, Presentable {
     func popToRoot  (animated: Bool)
 }
 
-final public class Router: NSObject, Routable {
+public class Router: NSObject, Routable {
 
     public let navigationController: UINavigationController
 
